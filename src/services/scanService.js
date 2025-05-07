a2c63f52cda1248a035e0469b4c3052ee981f925
Edward Ojeda <edward.ojeda.es@gmail.com>
2025-05-07 15:23:54 -0500

    Implement: funcionality scan social media

---
+++ src/services/scanService.js
@@ -0,0 +1,33 @@
+export const scanSocialMedia = async ({ nombre, apellido, username }) => {
+  const token = await generateToken();
+
+  const url = process.env.FLARE_API_BASE_URL
+
+  const query = `${nombre} ${apellido} @${username}`;
+
+  const body = {
+    query: {
+      text: query,
+    },
+    size: 20,
+    from: "0",
+    order: desc,
+    filters: {
+      severity: ["low"],
+      type: ["social_media"],
+    }
+  }
+
+  const response = await axios.post(
+    url,
+    body,
+    {
+      headers: {
+        Authorization: `Bearer ${token}`,
+        'Content-Type': 'application/json',
+      }
+    }
+  )
+
+  return response.data
+}
